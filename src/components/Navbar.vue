65604002a7e805fe18582f27e3393a4ef1703b73
serdargoleli <web.serdargoleli@gmail.com>
2023-02-02 23:45:06 +0300

    Menu açma ve kapatma ikonları eklendi.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,18 +7,15 @@
             <img src="/logo.svg" alt="" />
           </div>
         </div>
-
         <div class="col-6 d-flex justify-content-end d-lg-none">
           <div class="mobil-button-box">
-            <button class="btn-open-menu" @click="menuToggle">{{ isOpenMobileMenu }}</button>
+            <button class="btn-open-menu" @click="menuToggle"><Bars3Icon class="icon" /></button>
           </div>
         </div>
-
         <div class="overlay" @click="menuToggle" v-if="isOpenMobileMenu"></div>
-
         <div class="col-lg-7">
           <nav :class="`menu ${isOpenMobileMenu ? 'mobile-menu-show' : ''}`">
-            <a class="btn-close-menu" @click.prevent="menuToggle">X</a>
+            <a class="btn-close-menu" @click.prevent="menuToggle"> <XMarkIcon class="icon" /> </a>
             <a href="#" class="menu-item active">Free Design</a>
             <a href="#" class="menu-item">Lisance</a>
             <a href="#" class="menu-item">Articles</a>
@@ -26,7 +23,6 @@
             <a href="#" class="menu-item">About</a>
           </nav>
         </div>
-
         <div class="col-lg-3">
           <div class="buttons">
             <button class="button btn-ghost me-1">Login</button>
@@ -40,7 +36,7 @@
 
 <script setup>
   import { ref } from "vue";
-
+  import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/solid";
   const isOpenMobileMenu = ref(false);
 
   const menuToggle = () => {
