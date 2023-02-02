fa4406beb5483c10532fb4471c113505f1503afd
serdargoleli <web.serdargoleli@gmail.com>
2023-02-02 18:26:45 +0300

    init commit
    - vue projesi oluşturuldu.
    - bootstrap 5 ve normalize css kütüphaneleri eklendi

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "normalize.css";
+import "bootstrap/dist/css/bootstrap-grid.css";
+import "./assets/css/main.css";
+
+createApp(App).mount("#app");
