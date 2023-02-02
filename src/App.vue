326f7926a77e9777cd3d82a4430066b44099c280
serdargoleli <web.serdargoleli@gmail.com>
2023-02-03 01:37:35 +0300

    Data oluşturuldu
    - Sayfada göstermek üzere statik veriler oluşturuldu ve app.vue'ya import edildi.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
-<script setup>
-import Navbar from '@/components/Navbar.vue'
-</script>
-
 <template>
   <Navbar />
+  <Hero>
+    <input type="text" placeholder="search" v-model="searchText" />
+  </Hero>
 </template>
-
-
+<script setup>
+  import Navbar from "@/components/Navbar.vue";
+  import Hero from "@/components/Hero.vue";
+  import { ref, reactive } from "vue";
+  import { data } from "./data";
+  const searchText = ref("");
+  const posts = data();
+  console.log(posts);
+</script>
